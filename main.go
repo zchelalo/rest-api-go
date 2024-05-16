d22371d89bf0560699ede1203b91a91d63a716ec
zchelalo <eduardosaavedra687@gmail.com>
2024-05-16 01:35:39 -0700

    modify: server format

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -16,11 +17,15 @@ func main() {
 	router.HandleFunc("GET /courses", getCourses)
 	router.HandleFunc("GET /courses/{id}", getCourse)
 
-	err := http.ListenAndServe(port, router)
-
-	if err != nil {
-		log.Fatal(err)
+	server := &http.Server{
+		// Handler:      http.TimeoutHandler(router, 5*time.Second, "Timeout!"),
+		Handler:      router,
+		Addr:         fmt.Sprintf("127.0.0.1%s", port),
+		WriteTimeout: 5 * time.Second,
+		ReadTimeout:  5 * time.Second,
 	}
+
+	log.Fatal(server.ListenAndServe())
 }
 
 func getUsers(w http.ResponseWriter, req *http.Request) {
